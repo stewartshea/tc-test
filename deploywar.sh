a2c9f5937324b4349b749229fde1a57f6828b883
stewartshea <shea.stewart@arctiq.ca>
2017-03-05 12:33:26 -0500

    x

--- deploywar.sh
+++ deploywar.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 curl -v -u admin:admin123 nexus-openshift-docker.contactmanager.svc.cluster.local:8081/content/repositories/cctest/ab.war --output /usr/local/tomcat/webapps/ab.war
-/bin/sh -c /usr/local/tomcat/catalina.sh 
