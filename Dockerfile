e5830f9fee8605d3ecd3246a74247fbd62995ed6
stewartshea <shea.stewart@arctiq.ca>
2017-03-05 02:37:26 -0500

    x

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ COPY tomcat-users.xml /usr/local/tomcat/conf/tomcat-users.xml
 ADD deploywar.sh /root/deploywar.sh
 
 RUN \
-  /bin/sh /root/deploywar.sh
+  /bin/bash /root/deploywar.sh
 
 EXPOSE 8080
 CMD ["catalina.sh", "run"]
--- deploywar.sh
+++ deploywar.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -v -u admin:admin123 http://nexus-openshift-docker.contactmanager.svc.cluster.local:8081/content/repositories/cctest/ab.war --output /usr/local/tomcat/webapps/ab.war
+curl -v -u admin:admin123 nexus-openshift-docker.contactmanager.svc.cluster.local:8081/content/repositories/cctest/ab.war --output /usr/local/tomcat/webapps/ab.war
